93d2c2a6c0760213b15f22abcadcbb0ce3789dac
Nathan Stoddard <nstodda@purdue.edu>
2014-04-29 17:46:32 -0400

    Finish createUser function that calls the server in order to create the user. I haven't tested it yet though.

--- Todo/src/com/purdue/Todo/SplashActivity.java
+++ Todo/src/com/purdue/Todo/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.purdue.Todo;
 
 import android.app.Activity;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,67 +10,97 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
+import android.content.Intent;
+import android.widget.Button;
 
 public class SplashActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    Button loginButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open course chooser activity
+                Intent intent = new Intent(getApplicationContext(), CourseChooserActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private String serverURL = "http://54.213.80.211/ToDoList252/server/";
 
-    //TODO: this doesn't work yet
-    /**
-     * Sends a CreateUser request to the server. Currently only prints a debug message.
-     *
-     * @param courses
-     * @param colors
-     */
-    public void createUser(String[] courses, int[] colors) {
-        HttpClient client = new DefaultHttpClient();
-        String request = serverURL + "CreateUser.php?";
-        for (int i = 0; i < courses.length; i++) {
-            request += "course" + i + "=" + courses[i];
-            request += "color" + i + "=" + colors[i];
+    final SplashActivity context = this; //This is needed for GetUserTask
+
+    private class CourseAndColor {
+        public String course;
+        public int color; //a 6-hex-digit color (e.g. 0xff0000)
+
+        public CourseAndColor(String course, int color) {
+            this.course = course;
+            this.color = color;
         }
-        Toast.makeText(this, request, Toast.LENGTH_LONG).show();
-        /*HttpGet get = new HttpGet(request);
-        try {
-            HttpResponse response = client.execute(get);
-            if (response != null) {
-                //decode it
-            }
-        } catch (ClientProtocolException e) {
-            Toast.makeText(this, "Caught ClientProtocolException", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Toast.makeText(this, "Caught IOException", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(this, "Caught Exception", Toast.LENGTH_SHORT).show();
-        }*/
     }
 
-    final SplashActivity context = this;
+    private class CreateUserTask extends AsyncTask<CourseAndColor, Void, String> {
+        protected String doInBackground(CourseAndColor... courses) {
+            String request = serverURL + "CreateUser.php?";
+            for (int i = 0; i < courses.length; i++) {
+                request += "course" + i + "=" + courses[i].course;
+                request += "color" + i + "=" + courses[i].color;
+            }
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet get = new HttpGet(request);
+            try {
+                HttpResponse response = client.execute(get);
+                if (response != null) {
+                    InputStream content = response.getEntity().getContent();
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    String responseString = "";
+                    while ((s = buffer.readLine()) != null) {
+                        responseString += s;
+                    }
+                    return responseString;
+                } else {
+                    return "<null response>";
+                }
+            } catch (Exception e) {
+                Log.e("TODO list", e.toString());
+                return "<" + e.toString() + ">";
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            //TODO: handle the result
+            //The result will be enclosed in <> if it's an error, otherwise it's a JSON string
+            Log.i("TODO list", "Created user: " + result);
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
+    }
 
     private class GetUserTask extends AsyncTask<Integer, Void, String> {
         protected String doInBackground(Integer... params) {
             int userID;
             try {
                 userID = params[0];
-            }
-            catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 return "<Invalid number>";
             }
             HttpClient client = new DefaultHttpClient();
@@ -88,8 +119,7 @@ public class SplashActivity extends Activity {
                         responseString += s;
                     }
                     return responseString;
-                }
-                else {
+                } else {
                     return "<null response>";
                 }
             } catch (Exception e) {
@@ -101,22 +131,23 @@ public class SplashActivity extends Activity {
         protected void onPostExecute(String result) {
             //TODO: handle the result
             //The result will be enclosed in <> if it's an error, otherwise it's a JSON string
+            Log.i("TODO list", "Got user: " + result);
             Toast.makeText(context, result, Toast.LENGTH_LONG).show();
         }
     }
 
-    public void getUser(final int userID) {
+    public void login(View view) {
+        int userID = Integer.parseInt(((EditText) findViewById(R.id.userID)).getText().toString());
         new GetUserTask().execute(userID);
     }
 
-    public void login(View view) {
-        int userID = Integer.parseInt(((EditText) findViewById(R.id.userID)).getText().toString());
-        getUser(userID);
+    public void signUp(View view) {
+        //TODO: this should switch to a new Activity that allows the user to enter a list of courses and their colors, and then calls createUser
+
+        //createUser(new CourseAndColor("abc", 0xff0000), new CourseAndColor("def", 0x00ff00), new CourseAndColor("ghi", 0x0000ff));
     }
 
-    public void signUp(View view) {
-        String[] a = {"abc", "def", "ghi"};
-        int[] b = {0xff0000, 0x00ff00, 0x0000ff};
-        createUser(a, b);
+    private void createUser(CourseAndColor... courses) {
+        new CreateUserTask().execute(courses);
     }
 }
