307a924e5dea20ada3d650636ad8c30bab0c3f21
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-05-06 22:09:33 -0400

    Post

--- server/CreateUser.php
+++ server/CreateUser.php
@@ -2,12 +2,12 @@
 include 'User.php';
 $User= new User(NULL);
 for($i=0;true;$i++) {
-	if(isset($_GET["Course".$i])) {
-		if(isset($_GET["Color".$i])) {
-			$User->addCourse($_GET["Course".$i],$_GET["Color".$i]);
+	if(isset($_POST["Course".$i])) {
+		if(isset($_POST["Color".$i])) {
+			$User->addCourse($_POST["Course".$i],$_POST["Color".$i]);
 		}
 		else {
-			$User->addCourse($_GET["Course".$i],NULL);
+			$User->addCourse($_POST["Course".$i],NULL);
 		}
 	}
 	else {
